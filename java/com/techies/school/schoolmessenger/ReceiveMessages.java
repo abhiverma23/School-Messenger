70f559e78468673a2aae123656e4f454b0acc1b3
Abhishek Verma <www.abhiverma23@gmail.com>
2016-04-11 09:51:05 +0530

    Intermidate point

--- java/com/techies/school/schoolmessenger/ReceiveMessages.java
+++ java/com/techies/school/schoolmessenger/ReceiveMessages.java
@@ -1,13 +1,62 @@
 package com.techies.school.schoolmessenger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class ReceiveMessages extends AppCompatActivity {
+
+    private RadioButton a,p,t,m;
+
+    private String [] msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_messages);
+
+        a = (RadioButton) findViewById(R.id.receive_all);
+        p = (RadioButton) findViewById(R.id.receive_parents);
+        t = (RadioButton) findViewById(R.id.receive_teachers);
+        m = (RadioButton) findViewById(R.id.receive_management);
+
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p.setChecked(false);
+                t.setChecked(false);
+                m.setChecked(false);
+            }
+        });
+
+        p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setChecked(false);
+                t.setChecked(false);
+                m.setChecked(false);
+            }
+        });
+
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setChecked(false);
+                p.setChecked(false);
+                m.setChecked(false);
+            }
+        });
+
+        m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setChecked(false);
+                p.setChecked(false);
+                t.setChecked(false);
+            }
+        });
+
+
     }
 }
